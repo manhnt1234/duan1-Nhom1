761245bde1a42bcd6515526ea5b1918abc520cd3
HoangTue1998 <81170185+HoangTue1998@users.noreply.github.com>
2021-11-03 23:58:13 +0700

    update

--- src/duan1/nhom1/Duan1Nhom1.java
+++ src/duan1/nhom1/Duan1Nhom1.java
@@ -15,7 +15,7 @@ public class Duan1Nhom1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Chào bạn");
     }
     
 }
