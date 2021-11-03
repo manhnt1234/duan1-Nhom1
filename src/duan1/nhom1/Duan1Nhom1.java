e4d36ab0e26f4dc1f62c64f741f704060bfa84ce
HoangTue1998 <81170185+HoangTue1998@users.noreply.github.com>
2021-11-04 00:01:57 +0700

    update code

--- src/duan1/nhom1/Duan1Nhom1.java
+++ src/duan1/nhom1/Duan1Nhom1.java
@@ -15,7 +15,7 @@ public class Duan1Nhom1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Chào bạn");
+        System.out.println("Mọi người cố gắng nhé ");
     }
     
 }
